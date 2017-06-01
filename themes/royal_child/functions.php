0334950bbf490dbee0015fef1a80d56e99040304
LUCIANOPC\Luciano Nooijen\GitKraken <lucianonooijen@gmail.com>
2017-06-02 01:43:16 +0200

    Beta version of the website
    Updated for the beta version of the website

--- themes/royal_child/functions.php
+++ themes/royal_child/functions.php
@@ -100,3 +100,114 @@ function admin_style() {
   wp_enqueue_style('admin-styles', get_stylesheet_directory_uri().'/admin.css');
 }
 add_action('admin_enqueue_scripts', 'admin_style');
+
+/*
+|----------------------------------------------------
+|  WordPress: More unique slugs for attachments
+|----------------------------------------------------
+*/
+
+add_filter( 'wp_unique_post_slug', 'unique_post_slug', 10, 6 );
+function unique_post_slug( $slug, $post_ID, $post_status, $post_type, $post_parent, $original_slug ) {
+  if ( 'attachment' == $post_type )
+    $slug = $original_slug . uniqid( '-' );
+  return $slug;
+}
+
+/*
+|----------------------------------------------------
+|  WooCommerce: Change format of category loop title
+|----------------------------------------------------
+*/
+
+function woocommerce_template_loop_category_title( $category ) {
+?>
+	<h2 class="woocommerce-loop-category__title">
+		<?php
+    echo $category->name;
+
+    if ( $category->count > 0 )
+      echo apply_filters( 'woocommerce_subcategory_count_html', '', $category );
+  ?>
+	</h2>
+	<?php
+}
+
+/*
+|------------------------------------------------------
+|  WordPress: Change the Login Logo
+|------------------------------------------------------
+*/
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(/wp-content/uploads/logo.png);
+            width: 320px;
+            background-size: 160px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+/*
+|------------------------------------------------------
+|  Register custom sidebar: WooCommerce Product Filter
+|------------------------------------------------------
+*/
+
+add_action( 'widgets_init', 'royal_widgets_init' );
+function royal_widgets_init() {
+    register_sidebar( array(
+        'name' => __( 'WooCommerce Product Filter', 'royal' ),
+        'id' => 'wcpf',
+        'description' => __( 'Positie boven content voor WooCommerce Product Filter', 'royal' ),
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+	'after_widget'  => '</li>',
+	'before_title'  => '<h3 class="widgettitle">',
+	'after_title'   => '</h3>',
+    ) );
+}
+
+/*
+|------------------------------------------------------
+|  WooCommerce:  Rename 'You may also like'
+|------------------------------------------------------
+*/
+
+add_filter('gettext', 'translate_like');
+add_filter('ngettext', 'translate_like');
+function translate_like($translated)
+{
+	$translated = str_ireplace('You may also like&hellip;', 'Accessoires', $translated);
+	return $translated;
+}
+
+/*
+|------------------------------------------------------
+|  WooCommerce: Rename 'Related Products'
+|------------------------------------------------------
+*/
+
+add_filter('gettext', 'translate_related');
+add_filter('ngettext', 'translate_related');
+function translate_related($translated)
+{
+	$translated = str_ireplace('Related Products', 'Same Collection', $translated);
+	return $translated;
+}
+
+/*
+|------------------------------------------------------
+|  GeneratePress: Show Blog Page Header on WooCommerce
+|------------------------------------------------------
+*/
+
+add_filter( 'generate_get_blog_page_header','woocommerce_blog_page_header' );
+function woocommerce_blog_page_header() {
+    if ( function_exists( 'is_shop' ) && is_shop() ) {
+        return true;
+    }
+
+    return false;
+}
--- themes/royal_child/header.php
+++ themes/royal_child/header.php
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	<?php do_action( 'generate_before_header' ); ?>
 	<?php do_action( 'generate_header' ); ?>
 	<?php do_action( 'generate_after_header' ); ?>
-	
+
 	<div id="page" class="hfeed site grid-container container grid-parent">
 		<div id="content" class="site-content">
 			<?php do_action( 'generate_inside_container' ); ?>
