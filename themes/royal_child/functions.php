6fcc370ac49ead8e17b7c968c147c8bc012c7423
LUCIANOPC\Luciano Nooijen\GitKraken <lucianonooijen@gmail.com>
2017-07-05 00:42:50 +0200

    Final edits before dev to live transfer

--- themes/royal_child/functions.php
+++ themes/royal_child/functions.php
@@ -473,3 +473,50 @@ add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_p
 remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
 
 add_action( 'woocommerce_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
+
+/*
+|------------------------------------------------------
+|  WordPress: Add URL Slugs as Body Classes
+|------------------------------------------------------
+*/
+
+	// this only applies for non-admin pages
+	if ( !is_admin() && !function_exists( 'add_body_class' ) ) :
+
+
+		//	Add deconstructed URI as <body> classes
+		//	$classes = array of classes WP is already planning to add
+		function add_body_class( $classes )  {
+
+			// get the global post variable
+			global $post;
+
+			// make sure we're on a post page
+			if ($post && $post->ID) :
+
+				// loop through any categories
+				foreach( get_the_category($post->ID) as $category ) {
+					// and push the trimmed version to the $classes array
+					$classes[] = trim( $category->category_nicename );
+				}
+
+				// get the current page's URI (the part _after_ your domain name)
+				$uri = $_SERVER["REQUEST_URI"];
+				// explode that string into an array of "pieces"
+				$bodyclass = explode('/',$uri);
+				// loop through those pieces and push each into the $classes array
+				foreach($bodyclass as $category) {
+					$classes[] = trim($category);
+				}
+
+			endif; // $post...
+
+			// return a unique-onlye version of the array to WP
+			return array_unique($classes);
+		
+		}
+
+		add_filter('post_class', 'add_body_class');
+		add_filter('body_class', 'add_body_class');
+
+	endif; // !is_admin...
